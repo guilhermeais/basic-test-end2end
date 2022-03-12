e5df880b27d797b471588c706796421efc84a25f
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-11 23:47:08 -0300

    Cria métodos da rota

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
 import {  createServer  } from 'http'
+import { once } from 'events'
+
+const Database = new Map()
+
+function responseJSON(response, data){
+  response.writeHead(200, {'Content-Type': 'application/json'})
+  response.end(JSON.stringify(data))
+}
 
 async function handler(request, response){
-  response.end('Hello World')
+  const {method} = request
+  const methods = {
+    GET: async () => {
+      return responseJSON(response, [...Database.values()])
+    },
+    POST: async () => {
+      const body = await once(request, 'data')
+      const bodyParsed = JSON.parse(body)
+      console.log('data received: ',bodyParsed)
+
+      return responseJSON(response, {success: true})
+    },
+    DELETE: async () => {
+      return responseJSON(response, {success: true})
+    }
+  }
+
+  return methods[method]()
 }
 
 export default createServer(handler)

e12ecec50b9d43e7562d0315b83ec10b82c36f44
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-12 00:12:05 -0300

    Adiciona to-dos nos testes

---
+++ test/e2e/api.test.js
@@ -0,0 +1,7 @@
+import {jest, expect, test, describe} from '@jest/globals'
+
+describe('API E2E Test Suite', () => {
+  test.todo('GET / - should return and array')
+  test.todo('POST / - should save and item and return success')
+  test.todo('DELETE / - should delete an item and return success')
+});

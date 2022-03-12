c86b37175ff9067d1ba53109aa9ed35a5a2e4b97
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-12 01:16:55 -0300

    Cria um servidor para cada teste

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -1,7 +1,36 @@
-import {jest, expect, test, describe} from '@jest/globals'
+import {jest, expect, test, describe, beforeEach, afterEach} from '@jest/globals'
+
+import supertest from 'supertest'
+import server from '../../src/server';
 
 describe('API E2E Test Suite', () => {
-  test.todo('GET / - should return and array')
-  test.todo('POST / - should save and item and return success')
+  let request;
+  beforeEach(async ()=> {
+    request = ()=> supertest(server)
+  })
+
+    test('GET / - should return and array', async () => {
+    const response = await request()
+    .get('/')
+    const data = JSON.parse(response.text)
+    expect(response.status).toBe(200)
+    expect(data).toBeInstanceOf(Array)
+    expect(data.length).toEqual(0)
+  })
+
+  test('POST / - should save and item and return success', async () => {
+    const response = await request()
+    .post('/')
+    .send({
+      nome: 'guilherme',
+      age: 18
+    })
+    const expectedResponse = {success: true}
+    const data = JSON.parse(response.text)
+
+    expect(response.status).toBe(200)
+    expect(data).toStrictEqual(expectedResponse)
+  })
+
   test.todo('DELETE / - should delete an item and return success')
 });
